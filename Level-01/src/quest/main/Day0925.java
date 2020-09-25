ec940b3574a58292e2186e2387958a329423ac10
parkjye <parkjye44@gmail.com>
2020-09-25 21:39:12 +0900

    <Refactor> 가운데 글자 가져오기
    * Scanner로 문자열 입력받아서 실행할 수 있게 수정했습니다.

--- Level-01/src/quest/main/Day0925.java
+++ Level-01/src/quest/main/Day0925.java
@@ -5,18 +5,12 @@ import java.util.Scanner;
 public class Day0925 {
 	public static void main(String[] args) {
 		
-		//test Data
-		System.out.println(solution("abcdefghi"));
-		System.out.println(solution("qwer"));
-		
-		/*
 		Scanner scan = new Scanner(System.in);
 		System.out.println("문자를 입력하세요");
 		
 		String str = scan.next();
-		solution(str);
+		System.out.println(solution(str));
 		scan.close();
-		*/
 		
 	}
 	
@@ -27,7 +21,6 @@ public class Day0925 {
         //exception
         if(str.length()<1 || str.length()>100) {
         	System.out.println("입력한 글자가 100자 초과입니다. 종료.");
-     
         	return answer;
         }
         
