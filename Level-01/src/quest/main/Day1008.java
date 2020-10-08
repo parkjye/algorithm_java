ad59a8bc52cdf6f8a653acc8479d717165145108
parkjye <parkjye44@gmail.com>
2020-10-09 01:24:53 +0900

    <Refactor> 월간 코드 챌린지 시즌1, Quiz 1 리팩토링
    입력받은 정수 n을 3진법으로 나타낸 후 다시 뒤집어서 10진법으로 표현

--- Level-01/src/quest/main/Day1008.java
+++ Level-01/src/quest/main/Day1008.java
@@ -1,49 +1,55 @@
 package quest.main;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Day1008 {
 	public static void main(String[] args) {
-		/* n을 3진법 상에서 앞뒤로 뒤집은 후 다시 10진법으로 표현 */
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("정수를 입력하세요");
+		int n = scan.nextInt();
+		scan.close();
+		
+		//
+		if(isValid(n)) {
+			System.out.println(solution(n));
+		}
+	}
+	
+	
+	/* 10진법 -> 3진법 -> 10진법 */
+	public static int solution(int n) {
 		
 		int answer = 0;
-		int n = 125;
 		
-		//몫, 나머지
-		int quotient = 0;
-		int remainder = 0;
-		
-		//3진법 담을 문자열
-		String getBase3 = "";
-		
+		//나머지 담을 list
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		
-		if(isValid(n)) {
-			while(n>0) {
-				//입력받은 정수를 3으로 누고 몫과 나머지를 각각 저장한다.
-				quotient = n/3;
-				remainder = n%3;
-				
-				//정수 값을 몫으로 변경
-				n = quotient;
-				
-				//나머지는 list에 넣어준다.
-				list.add(remainder);
-			}
+		//(변경될)몫을 담을 변수
+		int quotient = 0;
+
+		//3진법 전체 조회할 문자열
+		String getBase3 = "";
+
+		while(n>0) {
+			//입력받은 정수를 3으로 나누고 나머지는 몫과 나머지를 각각 담는다. 
+			quotient = n/3;
+			list.add(n%3);
 			
-			//list에 저장한 값을 전체 조회한다.
-			for(int getInt : list) {
-				getBase3 += getInt;
-			}
-			
-			//parseInt(String, 진법)를 활용해서 10진법으로 변경한다.
-			answer = Integer.parseInt(getBase3, 3);
-			
-			System.out.println(answer);
-		}else {
-			System.out.println("n은 1이상 100,000,000이하");
+			//몫을 변경해준다.
+			n = quotient;
 		}
 		
+		//list에 저장된 나머지 값을 전체 조회한다.
+		for(int getInt : list) {
+			getBase3 += getInt;
+		}
+		
+		//parseInt(String, 진법)를 활용해서 10진법으로 변경한다.
+		answer = Integer.parseInt(getBase3, 3);
+
+		return answer;	
 	}
 	
 	/* condition */
