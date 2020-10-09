9ded9e78c845b4349de31265b0bdecf0a2b24cca
parkjye <parkjye44@gmail.com>
2020-10-10 01:41:48 +0900

    <Refactor> 평균 구하기

--- Level-01/src/quest/main/Day1007.java
+++ Level-01/src/quest/main/Day1007.java
@@ -1,12 +1,6 @@
 package quest.main;
 
 public class Day1007 {
-	public static void main(String[] args) {
-		
-		int[] arr = {1, 2, 3, 4, 5};
-		System.out.println(solution(arr));
-		
-	}
 	
 	/* 평균 구하기 */
 	public static double solution(int[] arr) {
@@ -22,4 +16,21 @@ public class Day1007 {
         return answer;
     }
 	
+	/* Condition */
+	public static boolean isValid(int[] arr) {
+		boolean flag=false;
+		
+		if(arr.length >= 1 || arr.length<=100) flag=true;
+		
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] >= -10000 || arr[i] <= 1000) flag=true;
+		}
+		
+		return flag;
+	} 
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 10000};
+		if(isValid(arr)) System.out.println(solution(arr));
+	}
 }
