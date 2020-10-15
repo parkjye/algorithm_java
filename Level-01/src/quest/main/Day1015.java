bfe33fa44b8edc7d4c246cb9a45b11f0a6fd4657
parkjye <parkjye44@gmail.com>
2020-10-15 18:47:29 +0900

    <Feat> 정수 제곱근 판별

---
+++ Level-01/src/quest/main/Day1015.java
@@ -0,0 +1,41 @@
+package quest.main;
+
+public class Day1015 {
+	
+	/* condition */
+	public static boolean isValid(long n) {
+		boolean flag = false;
+		if(n>=1 && n<=50000000000000L) flag=true;
+		return flag;
+	}
+	
+	/* 정수 제곱근 판별 */
+	public static long solution(long n) {
+        long answer = 0;
+        double root = 0;
+        int point = 0;
+        
+        //n의 제곱근 구하기
+        root = (double)Math.sqrt(n);
+        
+        //소수점 첫 번째 자리
+        point = (int)(root*10)%10;
+        
+        if(point==0) {
+        	answer = (long)Math.pow(root+1, 2);
+        }else {
+        	answer = -1;
+        }
+
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		long n = 121;
+		
+		if(isValid(n)) {
+			System.out.println(solution(n));
+		}
+		
+	}
+}
