2074145c3f303163ceb3ede67f3a8c5eeea3ad06
parkjye <parkjye44@gmail.com>
2020-10-23 00:11:05 +0900

    <Feat> 콜라츠 추측

---
+++ Level-01/src/quest/main/Day1023.java
@@ -0,0 +1,31 @@
+package quest.main;
+
+public class Day1023 {
+	/* 콜라츠 추측  */
+	public static int solution(double num) {
+		int answer=0;
+		
+		for(int i=0; i<=500; i++) {
+			if(num==1) break;
+			
+			num = (num%2==0) ? num/2 : num*3+1;
+			answer++;
+			
+			if(i==500) answer=-1;
+		}
+
+		return answer;
+	}
+	
+	/* condition */
+	public static boolean isValid(int num) {
+		boolean flag=false;
+		if(num>=1 && num<8000000) flag=true;
+		return flag;
+	}
+	
+	public static void main(String[] args) {
+		int num = 626331;
+		if(isValid(num)) System.out.println(solution(num));
+	}
+}
