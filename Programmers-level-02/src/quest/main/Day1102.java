30e5f412ead8111801f094e2686252710ebf4e50
parkjye <parkjye44@gmail.com>
2020-11-02 22:16:53 +0900

    <Fix> 짝지어 제거하기
    * class 변경
    
    * 조건 추가
        글자수가 홀수면 제거가 불가능하기 때문에 return answer
    
    * 조건 추가 2
        stack안에 있는 값과 현재 비교하는 문자값이 동일하지 않을 경우,
        현재 문자값을 stack에 문자 추가
    
    * 문제 내의 제한 사항 추가
        1. 문자열의 길이는 1,000,000 이하의 자연수
        2. 문자열은 모두 소문자
    
    * main()에 예시변경 및 함수 호출

--- Programmers-level-02/src/quest/main/Day1102.java
+++ Programmers-level-02/src/quest/main/Day1102.java
@@ -4,27 +4,46 @@ import java.util.Stack;
 
 public class Day1102 {
 	/* 짝지어 제거하기 */
-	public static int solution(String s) {
+	public int solution(String s) {
 		int answer=0;		
 		Stack<Character> stack = new Stack<Character>();
 		
-		for(int i=0; i<s.length(); i++) {
-			if(stack.isEmpty()) {
-				stack.push(s.charAt(i));
+		if (s.length() % 2 != 0) {
+			return answer;
+			
+		}else {
+			for(int i=0; i<s.length(); i++) {
 				
-			}else if(!stack.isEmpty() && stack.peek() == s.charAt(i)){
-				stack.pop();
-				continue;
+				if(stack.isEmpty()) stack.push(s.charAt(i));
+				else if(!stack.isEmpty() && stack.peek() == s.charAt(i)) stack.pop();
+				else stack.add(s.charAt(i));
 			}
+			
+			if(stack.isEmpty()) answer = 1;
 		}
-		
-		if(stack.isEmpty()) answer = 1;
 		
 		return answer;
 	}
+	
+	public static boolean isValid(String s) {
+		boolean flag=false;
+		
+		if(s.length()<=1000000) {
+			for(int i=0; i<s.length(); i++) {
+				if(Character.isLowerCase(s.charAt(i))) flag=true;
+			}
+		}
+
+		return flag;
+	}
 
 	public static void main(String[] args) {
-		String s = "cdcd";
-		System.out.println(solution(s));
+		String s = "baabaa";
+		
+		Day1102 Solution = new Day1102();
+
+		if(isValid(s)) {
+			System.out.println(Solution.solution(s));
+		}
 	}
 }
