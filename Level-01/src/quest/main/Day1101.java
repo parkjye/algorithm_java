8583bc6e021ad5a41c391b2dfc145f1046d92429
parkjye <parkjye44@gmail.com>
2020-11-12 21:39:43 +0900

    <Fix> 서울에서 김서방 찾기
    if()에 break를 사용해서 속도를 개선합니다.

--- Level-01/src/quest/main/Day1101.java
+++ Level-01/src/quest/main/Day1101.java
@@ -6,7 +6,10 @@ public class Day1101 {
         int getNum = 0;
         
         for(int i=0; i<seoul.length; i++){
-            if("Kim".equals(seoul[i])) getNum = i;
+            if("Kim".equals(seoul[i])) {
+            	getNum = i;
+                break;
+            }
         }
         
         answer="김서방은 "+getNum+"에 있다";
