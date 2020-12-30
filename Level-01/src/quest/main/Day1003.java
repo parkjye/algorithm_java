75f6ad31d137d3ca19b747979197f6a0cbf88094
parkjye <parkjye44@gmail.com>
2020-12-31 00:49:15 +0900

    <Feat> 자릿수 더하기 (복습)

--- Level-01/src/quest/main/Day1003.java
+++ Level-01/src/quest/main/Day1003.java
@@ -1,27 +1,18 @@
 package quest.main;
 
-import java.util.Scanner;
-
 public class Day1003 {
+	/* 자릿수 더하기 */
 	public static void main(String[] args) {
+		Day1003 addNum = new Day1003();
+		int n = 123;
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("정수 입력");
-		int n = scan.nextInt();
-		scan.close();
-		
-		if(isValid(n)) {
-			System.out.println(solution(n));
-		}
-		
+		if(isValid(n)) System.out.println(solution(n));
+		if(isValid(n)) System.out.println(addNum.solution2(n));
 	}
 	
-	/* 자릿수 더하기 */
 	public static int solution(int n) {
         int answer = 0;
-
-        //입력받은 정수를 10으로 나눈 나머지를 계속 더해준다.
-        //n을 10으로 나눈 값을 다시 n에 넣어준다.
+        
     	while(n!=0) {
     		answer += n%10;
     		n /= 10;
@@ -30,10 +21,21 @@ public class Day1003 {
         return answer;
     }
 	
+	public int solution2(int n) {
+		int answer = 0;
+		String[] getSum = Integer.toString(n).split("");
+		
+		for(int i=0; i<getSum.length; i++) {
+			answer += Integer.parseInt(getSum[i]);
+		}
+		
+		return answer;
+	}
+	
     public static boolean isValid(int n) {
 		boolean flag=false;
 		if(n <= 100000000) flag=true;
 		return flag;
 	}
 	
-}//Day1003
+}
