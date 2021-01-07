ffb72aed5bf601284a1656a01cbe1453f8f62915
JYE <parkjye44@gmail.com>
2021-01-08 00:29:11 +0900

    <Feat> 소수찾기 (에라토스테네스의 체)
        * 소수가 되는 수의 배수들을 지우면 소수만 남는다.
            (2부터 자기 자신을 제외한 배수를 지운다.)
    
    1. 1은 소수이므로 2부터 n까지의 수를 나열한다.
    2. 2는 소수이므로 2를 제외한 2의 배수를 모두 0으로 변경한다.
    3. 0이 아닌 남은 수 중에 3은 소수이므로 3의 배수를 모두 0으로 변경한다.
    4. 위와 같은 식으로 5, 7 ... . 수도 반복한다.
    5. 작업이 끝나면 0이 아닌 수를 세어서 리턴한다.

--- Level-01/src/quest/main/Day0924.java
+++ Level-01/src/quest/main/Day0924.java
@@ -1,79 +1,26 @@
 package quest.main;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Day0924 {
+	/* 소수 찾기(에라토스테네스의 체) */
 	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		System.out.println("정수를 입력하세요");
-		int n = scan.nextInt();
-		
-		//solution(n);
-		solution2(n);
-		
-		scan.close();
+		int n = 10;
+		solution(n);
 	}
 	
-	public static int solution2(int n) {	
-		int answer = 0;
-		
-		//exception
-        if(n>1000000) {
-        	System.out.println("입력한 숫자가 1,000,000보다 큽니다. 종료합니다.");
-        	return answer;
-        }
-		
-        for (int i=2; i <= n; i++) {
-            boolean flag = false;
-            
-            for (int j=2; j<=i/2; j++) {
-                if (i % j == 0) {
-                	flag = true;
-                    break;
-                }
-            }
-            
-            //소수찾을때마다 ++
-            if (flag) {
-                answer++;
-            }
-        }
-        
-        return answer;
-	}
-	
-	
-	/********************************************/
 	public static int solution(int n) {
         int answer = 0;
-        int indexNum = 0;      
-
-        ArrayList<Integer> getList = new ArrayList<>();
-		//getList.add(1);
+        int[] numbers = new int[n+1];
         
-        //exception
-        if(n>1000000) {
-        	System.out.println("입력한 숫자가 1,000,000보다 큽니다. 종료합니다.");
-        	return answer;
-        }  
-        
-        for(int i=2; i<=n; i++) {
-        	
-        	indexNum = n%i;
-        	getList.add(indexNum);
+        for(int i=2; i<=n; i++) numbers[i]=i;
 
-        	//
-        	System.out.println("j["+i+"] "+indexNum);
+        for(int i=2; i<n; i++) {
+        	if(numbers[i]==0) continue;
         	
-        	//여기 다시
-        	for(int j=1; j>=n; j++) {
-	        	if(getList.get(0)==0 && getList.size()==0) {
-	        		System.out.println("소수: "+getList.get(indexNum));
-	        		
-	        	}
-        	}
+        	for(int j=2*i; j<=n; j+=i) numbers[j] = 0;
+        }
+        
+        for(int i=0; i<numbers.length; i++) {
+        	if(numbers[i]!=0) answer++;
         }
         
         return answer;
