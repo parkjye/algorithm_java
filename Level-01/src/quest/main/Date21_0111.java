4012f00fcfdc09d43728908441467cfa7ae8576f
JYE <parkjye44@gmail.com>
2021-01-11 22:28:06 +0900

    문자열 내 마음대로 정렬하기

--- Level-01/src/quest/main/Date21_0111.java
+++ Level-01/src/quest/main/Date21_0111.java
@@ -53,33 +53,5 @@ public class Date21_0111 {
 		
 		return answer;
 	}
-	
-	public String[] solution2(String[] strings, int n) {
-		String[] answer = {};
-		
-		//sun = 012
-		//스트링스의 첫번째 단어를 뽑아서 길이를 확인할까?
-		
-		char[] getChar = strings[0].toCharArray();
-		
-		System.out.println(getChar[0]);
-		System.out.println(getChar[1]);
-		System.out.println(getChar[2]);
-		System.out.println(getChar[3]);
-		
-		System.out.println(getChar.length); //각 단어의 길이를 알 수 있음
-		
-		char[] tmpArr = new char[strings.length];
-		
-		//for()
-		//위와같이 스트링스의 0번째 단어를 char배열에 하나씩 넣는다.
-		//그리고 그 중에 n번(2)째 철자를 map에 담기
-		//map은 <n번의 철자, strings에서 해당 글자가 포함된 인덱스>
-		
-		// map을 sort한다. map에서 <철자> 순서 예를 들어 2 1 0순으로 되면
-		// strings 배열의 2번의 값을 answer[0]에, 1번 값을 answer[1], 2번 값을 answer[2]에 넣고 리턴한다.
-		
-		return answer;
-	}
 
 }
