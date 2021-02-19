3af2f3f835dd26536e806ec8260b4d6bf9093e2d
JYE <parkjye44@gmail.com>
2021-02-19 19:26:01 +0900

    Level 1. 소수찾기 (에라토스테네스의 체)
    * 소수가 되는 수의 배수들을 지우면 소수만 남는다.
        (2부터 자기 자신을 제외한 배수를 지운다.)
    
    1. 1은 소수가 아니므로 2부터 n까지의 수를 나열한다.
    2. 2부터 소수의 배수들을 하나씩 지워나간다. (0으로 변경한다)
    3. 위와 같은 방식으로 3, 5, 7 ... . 수도 반복한다.
    4. 이 때 배열의 값이 이미 0으로 변경된 것은 넘어간다.
    5. 작업이 끝나면 0이 아닌 수를 세어서 리턴한다.

---
+++ Algo-programmers/src/level02/main/FindPrimeNumber.java
@@ -0,0 +1,33 @@
+package level02.main;
+
+public class FindPrimeNumber {
+	/* 소수찾기 */
+	public static void main(String[] args) {
+		FindPrimeNumber prime = new FindPrimeNumber();
+		
+		int n = 10;
+		System.out.println(prime.solution(n));
+	}
+
+	/* 에라토스테네스의 체 */
+	public int solution(int n) {
+		int answer = 0;
+		int[] numbers = new int[n+1];
+
+		for(int i=2; i<=n; i++) numbers[i]=i;
+
+		for(int i=2; i<n; i++) {
+			
+			if(numbers[i] == 0) continue;
+			
+			for(int j=2*i; j<=n; j+=i) numbers[j] = 0;
+		}
+		
+		for(int i=0; i<numbers.length; i++) {
+			
+			if(numbers[i] != 0) answer++;
+		}
+		
+		return answer;
+	}
+}
