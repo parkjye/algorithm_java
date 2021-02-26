2bbd9936421e38fa13a191690c8cc1282fdf553d
JYE <parkjye44@gmail.com>
2021-02-27 02:12:28 +0900

    :sparkles: Level 1. 이상한 문자 만들기
    * idx+1로 수정
    * 전에 통과했던 solution 추가

--- Algo-programmers/src/level01/main/CreateStrangeChar.java
+++ Algo-programmers/src/level01/main/CreateStrangeChar.java
@@ -7,6 +7,8 @@ public class CreateStrangeChar {
 		
 		String s = "try hello world";
 		System.out.println(createStr.solution(s));
+		System.out.println(createStr.solution2(s));
+
 	}
 	
 	public String solution(String s) {
@@ -16,11 +18,41 @@ public class CreateStrangeChar {
 		String[] arrStr = s.split("");
 		
 		for(String str : arrStr) {
-			idx = str.contains(" ") ? 0 : idx++;
+			idx = str.contains(" ") ? 0 : idx+1;
 			answer += idx%2 == 0 ? str.toLowerCase() : str.toUpperCase();
 		}
 		
 		return answer;
+	}
+	
+	public String solution2(String s) {
+		String answer = "";
+		int cnt = 0;
+		
+		String getStrArr[] = s.split("");
+		  
+		for(int i = 0; i<getStrArr.length; i++){
+			
+			if(getStrArr[i].equals(" ")){ 
+				cnt = 0;
+				
+			} else{
+				
+				if(cnt % 2 == 0){
+					cnt++;
+					getStrArr[i] = getStrArr[i].toUpperCase();
+					
+				} else{
+					cnt++;
+					getStrArr[i] = getStrArr[i].toLowerCase();
+					
+				}
+			}
+		
+			answer += getStrArr[i];
+		}
+		    
+	    return answer;
 	}
 
 }
